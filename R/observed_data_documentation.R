9a2bee6c03005ecf61ce2f881abc7c6c3d00c0ec
ronakrah <ronakrahmati25@gmail.com>
2021-12-28 20:33:15 +0100

    final modification with adding items to description done

--- R/observed_data_documentation.R
+++ R/observed_data_documentation.R
@@ -1,7 +1,7 @@
 
 #' @title observed Klementinum data
 #' @description the data include observed precipitation and temperature for Klementinum
-#' @format : a data table with 3 columns and 73322 rows that includes:
+#' @format a data table with 3 columns and 73322 rows that includes:
 #' \describe{
 #'   \item{DTM}{recording time}
 #'   \item{pr_o}{observed precipitation}
--- R/tem_raster_documentation.R
+++ R/tem_raster_documentation.R
@@ -1,5 +1,13 @@
 #' @title simulated  data
 #' @description the data include simulated temperature
-#' @format : a raster with dimension  16  17  1
+#' @format  a raster with dimension  16, 17, 272  (nrow, ncol, ncell)
+#'\describe{
+  #'   \item{z-value}{recorded time}
+  #'   \item{zvar}{simulated temprature}
+  #'   \item{band}{1  (of  55115  bands)}
+  #'   \item{resolution}{2.8125, 2.790426  (x, y)}
+  #'   \item{extent}{-1.40625, 46.40625, 27.90615, 72.55296  (xmin, xmax, ymin, ymax)}
+  #'   \item{crs}{+proj=longlat +datum=WGS84}
 
+  #'}
 "tem_raster"
