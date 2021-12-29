3806924525e0b0eae6cfcab74f94e68173a138c1
ronakrah <ronakrahmati25@gmail.com>
2021-12-29 21:30:16 +0100

    creating statistics of both data

---
+++ R/station_documentation.R
@@ -0,0 +1,14 @@
+
+#' @title  Klementinum station coordinates
+#' @description showing the coordinates (longitude and latitude) of Klementinum
+#' @format a data table with 2 columns and 1 row  that includes:
+#' \describe{
+#'   \item{lon}{longitude}
+#'   \item{lat}{latitude}
+#'
+#'}
+"station"
+
+
+
+
